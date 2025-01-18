f52016ba4e077057a719f6ce192c82fe512d704e
spearkkk <spearkkp@gmail.com>
2025-01-19 00:39:23 +0900

    [WIP] Implement updating

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"flag"
 	"log"
+	"os"
+	"os/exec"
 	"regexp"
 	"strings"
 
 	aw "github.com/deanishe/awgo"
+	"github.com/deanishe/awgo/update"
 	ts "github.com/spearkkk/u/timestamp"
 	uuid "github.com/spearkkk/u/uuid"
 )
@@ -17,11 +21,19 @@ type Utility interface {
 	GetResults(wf *aw.Workflow)
 }
 
+var updateJobName string = "update"
+var repo string = "spearkkk/u"
+var doCheck bool
+var query string
+
 var wf *aw.Workflow
 var utilities = []Utility{}
 
 func init() {
-	wf = aw.New()
+	flag.BoolVar(&doCheck, "check", false, "Check for updates")
+
+	wf = aw.New(update.GitHub(repo))
+
 	utilities = append(utilities, uuid.NewUUID())
 	utilities = append(utilities, ts.NewTimestamp("", ""))
 }
@@ -32,6 +44,47 @@ func main() {
 
 func run() {
 	log.Println("Running workflow...")
+
+	flag.Parse()
+	query = flag.Arg(0)
+
+	if wf.UpdateCheckDue() && !wf.IsRunning(updateJobName) {
+		log.Println("Running update check in background...")
+
+		cmd := exec.Command(os.Args[0], "-check")
+		if err := wf.RunInBackground(updateJobName, cmd); err != nil {
+			log.Printf("Error starting update check: %s", err)
+		}
+	}
+
+	// Only show update status if query is empty.
+	if query == "" && wf.UpdateAvailable() {
+		// Turn off UIDs to force this item to the top.
+		// If UIDs are enabled, Alfred will apply its "knowledge"
+		// to order the results based on your past usage.
+		wf.Configure(aw.SuppressUIDs(true))
+
+		// Notify user of update. As this item is invalid (Valid(false)),
+		// actioning it expands the query to the Autocomplete value.
+		// "workflow:update" triggers the updater Magic Action that
+		// is automatically registered when you configure Workflow with
+		// an Updater.
+		//
+		// If executed, the Magic Action downloads the latest version
+		// of the workflow and asks Alfred to install it.
+		wf.NewItem("Update available!").
+			Subtitle("↩ to install").
+			Autocomplete("workflow:update").
+			Valid(false)
+	}
+
+	// Add an extra item to reset update status for demo purposes.
+	// As with the update notification, this item triggers a Magic
+	// Action that deletes the cached list of releases.
+	wf.NewItem("Reset update status").
+		Autocomplete("workflow:delcache").
+		Valid(false)
+
 	// Parse input into queries
 	var queries []string
 	if len(wf.Args()) > 0 {
